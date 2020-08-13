8076d52169400ef6f1ae8e9cc2b3bd7b4dc22b4f
tsushima yuki <y.tsushima.sys20@morijyobi.ac.jp>
2020-08-14 02:12:41 +0900

    6の6章終わりました。

--- my_module.py
+++ my_module.py
@@ -3,9 +3,8 @@ def my_pow(x, y):
 if __name__ == "__main__":
     x, y, exp = 2, 5, 32
     ans = my_pow(x, y)
-    print("Test my_pow({}, {}) -> {}, exp: {}).format(x, y., ans, exp), end="")
+    print("Test my_pow({}, {} -> {}, exp: {}".format(x, y, ans, exp), end="")
     if ans == exp:
         print("Test OK")
     else:
-        pri
-        ("Test Fail"
+        print("Test Fail")
